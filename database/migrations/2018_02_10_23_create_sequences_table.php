7e524c3097d68a84e39edf06b16f93702f33d2d6
Kyle Eisenbarger <keisenb@ksu.edu>
2018-02-10 16:30:13 -0600

    Added additional migration files

---
+++ database/migrations/2018_02_10_23_create_sequences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSequencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sequences', function (Blueprint $table) {
+            $table->integer('level');
+            $table->integer('game_id')->unsigned();
+            $table->foreign('game_id')->references('id')->on('games');
+            $table->integer('order');
+            $table->integer('color_id')->unsigned();
+            $table->foreign('color_id')->references('id')->on('colors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sequences');
+    }
+}
